94f2d7a9a3546c81cf48c38e44d432bf32866782
VibhaGaddi <30654089+VibhaGaddi@users.noreply.github.com>
2017-08-17 14:53:41 +0100

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,9 @@ app.get('/', function (req, res) {
   c
 });
 
-
+app.get('/article-one', function (req, res) {
+    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+});
 app.get('/article-two', function (req, res) {
     res.send('Article two will be served here')
 });
