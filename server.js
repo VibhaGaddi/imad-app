0ac74425f9e71a7f17e3b17062a191e53872c9a5
VibhaGaddi <30654089+VibhaGaddi@users.noreply.github.com>
2017-08-18 13:22:11 +0100

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -6,10 +6,19 @@ var app = express();
 app.use(morgan('combined'));
 
 
-var content = {
+var articleOne = {
   title:'Article One | Vibha Gaddi',
   heading:'Article One',
   date:'August 17 2017',
+  content:` <p>
+                This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.
+            </p>
+            <p>
+                This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.
+            </p>
+            <p>
+                This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.
+            </p>`
 };
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, 'ui', 'index.html'));
